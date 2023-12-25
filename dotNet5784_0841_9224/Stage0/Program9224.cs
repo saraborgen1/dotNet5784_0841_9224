f84f5801b89423274caf1d6a345f4f2e7d76519d
Naama Leah Radonsky <radonsky@acad.jct.ac.il>
2023-12-25 14:46:54 +0200

    Initial code+ Welcome functions

---
+++ dotNet5784_0841_9224/Stage0/Program9224.cs
@@ -0,0 +1,17 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome9224();
+        Welcome0841();
+        Console.ReadKey();
+    }
+
+    private static void Welcome9224()
+    {
+        Console.WriteLine("Enter your name :");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0},welcome to my first console application", name);
+    }
+    static partial void Welcome0841();
+}
