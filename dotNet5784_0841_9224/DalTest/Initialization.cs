1d854c4d734fc60c6d9e8f5d1d0a9f04057ade49
שרה בורגן <s.b.01082004@gmail.com>
2024-01-04 15:59:24 +0200

    מספרים רנדומלים

--- dotNet5784_0841_9224/DalTest/Initialization.cs
+++ dotNet5784_0841_9224/DalTest/Initialization.cs
@@ -3,8 +3,11 @@ using DalApi;
 using DO;
 public static class Initialization
 {
+   
     private static ITask? s_dalTask; //stage 1
-
+    /// <summary>
+    /// Generating random numbers while filling in the object values
+    /// </summary>
     private static readonly Random s_rand = new();
 
     private static createTasks
