97fe93e6a13dc3e2eb71fa3d5b5c53781228f735
שרה בורגן <s.b.01082004@gmail.com>
2024-01-04 15:46:14 +0200

    הוספת הערות

--- dotNet5784_0841_9224/DalFacade/DO/Task.cs
+++ dotNet5784_0841_9224/DalFacade/DO/Task.cs
@@ -1,5 +1,20 @@
 ﻿namespace DO;
-
+/// <summary>
+/// A task entity represents the assigned tasks with everything included in the matter-times, output, notes, etc.
+/// </summary>
+/// <param name="Id">Unique ID number (automatic runner number)</param>
+/// <param name="Ailas">Name of the task</param>
+/// <param name="Description">A text detailing the task</param>
+/// <param name="IsMilestone">Boolean variable, in the first three steps, the DAL layer, its value will be False in all tasks. Later a task type entity will be able to represent a milestone and then its value can also be True.</param>
+/// <param name="StartDate">Indicates the time when the task was created by the administrator</param>
+/// <param name="ScheduledDate">Planned date for the start of work</param>
+/// <param name="DeadlineDate">Latest possible completion date for the work to be done</param>
+/// <param name="CompleteDate">Actual end date</param>
+/// <param name="RequiredEffortTime">The length of time to proceed with the task</param>
+/// <param name="Deliverables">A string describing the results or items provided at the end of the task</param>
+/// <param name="Remarks">Notes on the assignment</param>
+/// <param name="Engineerld">The engineer ID assigned to the task</param>
+/// <param name="Copmlexity">The difficulty level of the task</param>
 public record Task
 (
     int Id,
--- dotNet5784_0841_9224/DalList/EngineerImplementation.cs
+++ dotNet5784_0841_9224/DalList/EngineerImplementation.cs
@@ -5,6 +5,12 @@ using System.Collections.Generic;
 
 public class EngineerImplementation : IEngineer
 {
+    /// <summary>
+    /// Adding a new object of type Engineer to a database, (to the list of objects of type Engineer).
+    /// </summary>
+    /// <param name="item">A reference to an existing object of the Engineer type. The object was created in an upper layer and its fields are already filled with normal values.</param>
+    /// <returns>The method will return the running number of the newly created object in the list.</returns>
+    /// <exception cref="NotImplementedException">In case of an attempt to add an object that already exists - an exception will be thrown</exception>
     public int Create(Engineer item)
     {
         foreach (Engineer temp in DataSource.Engineers)
@@ -17,7 +23,11 @@ public class EngineerImplementation : IEngineer
         DataSource.Engineers.Add(item);
         return item.Id;
     }
-
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <exception cref="NotImplementedException"></exception>
     public void Delete(int id)
     {
         foreach (Engineer temp in DataSource.Engineers)
@@ -30,7 +40,11 @@ public class EngineerImplementation : IEngineer
         }
         throw new NotImplementedException($"An object of type Engineer with such an ID={id} does not exist");
     }
-
+    /// <summary>
+    /// Returning a reference to a single object of type Engineer with a certain ID
+    /// </summary>
+    /// <param name="id">ID number of an object</param>
+    /// <returns>If the object exists, the method will return a reference to the existing object. Otherwise, the method will return null.</returns>
     public Engineer? Read(int id)
     {
         foreach (Engineer item in DataSource.Engineers)
@@ -39,12 +53,19 @@ public class EngineerImplementation : IEngineer
         }
         return null;
     }
-
+    /// <summary>
+    /// Returning a copy of the list of references to all objects of type Engineer
+    /// </summary>
+    /// <returns>The method returns a new list that is a copy of the existing list of all objects of type Engineer.</returns>
     public List<Engineer> ReadAll()
     {
         return new List<Engineer>(DataSource.Engineers);
     }
-
+    /// <summary>
+    /// Update of an existing object. The update will consist of deleting the existing object with the same ID number and replacing it with a new object with the same ID number and updated fields.
+    /// </summary>
+    /// <param name="item">A reference to an updated existing object of type Engineer.</param>
+    /// <exception cref="NotImplementedException">If there is no object with the received ID number - an exception will be thrown</exception>
     public void Update(Engineer item)
     {
         foreach (Engineer engineer in DataSource.Engineers)
