67d9bd1ef1ea9b617181b4682c3526e84532a0d7
Naama Leah Radonsky <radonsky@acad.jct.ac.il>
2024-01-04 15:48:50 +0200

    עד לתיעוד של תלות ומשימות

--- dotNet5784_0841_9224/DalList/DependencyImplementation.cs
+++ dotNet5784_0841_9224/DalList/DependencyImplementation.cs
@@ -4,8 +4,14 @@ using DO;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
+
 public class DependencyImplementation : IDependency
 {
+    /// <summary>
+    /// Adding a new object of type Dependency to a database, (to the list of objects of type Dependency).
+    /// </summary>
+    /// <param name="item">A reference to an existing object of type Dependency.</param>
+    /// <returns>The method will return the running number of the newly created object in the list</returns>
     public int Create(Dependency item)
     {
         int newId = DataSource.Config.NextDependencyId;
@@ -13,7 +19,11 @@ public class DependencyImplementation : IDependency
         DataSource.Dependencys.Add(newDependency);
         return newId;
     }
-
+    /// <summary>
+    /// Deletion of an existing object with a certain ID, from the list of objects of type Dependency
+    /// </summary>
+    /// <param name="id">ID number of an object</param>
+    /// <exception cref="NotImplementedException">An attempt to delete an object that does not exist</exception>
     public void Delete(int id)
     {
         if (DataSource.Dependencys.Find(d => d.Id == id) == null)
@@ -23,17 +33,32 @@ public class DependencyImplementation : IDependency
         DataSource.Dependencys.Remove(DataSource.Dependencys.Find(d => d.Id == id)!);
 
     }
+    /// <summary>
+    /// Returning a reference to a single object of type Dependency with a certain ID, if it exists in 
+    ///a database (in a list of data of type Dependency), or null if the object does not exist.
+    /// </summary>
+    /// <param name="id">ID number of an object</param>
+    /// <returns>If there is an object in the database with the received ID number, the method
+    /// will return a reference to the existing object.Otherwise, the method will return null.</returns>
 
     public Dependency? Read(int id)
     {
         return DataSource.Dependencys.FirstOrDefault(d => d.Id == id);
     }
-
+    /// <summary>
+    /// Return a copy of the list of references to all objects of type Dependency
+    /// </summary>
+    /// <returns>The method returns a new list that is a copy of the existing list of all objects of type Dependency.</returns>
     public List<Dependency> ReadAll()
     {
         return new List<Dependency>(DataSource.Dependencys);
     }
-
+    /// <summary>
+    /// Update of an existing object. The update will consist of deleting the existing object with the same ID number and replacing it with 
+    /// a new object with the same ID number and updated fields.
+    /// </summary>
+    /// <param name="item">A reference to an updated existing object of type Dependency.</param>
+    /// <exception cref="NotImplementedException">In case there is no object with the received id</exception>
     public void Update(Dependency item)
     {
         if (DataSource.Dependencys.Find(d => d == item) == null) 
--- dotNet5784_0841_9224/DalList/TaskImplementation.cs
+++ dotNet5784_0841_9224/DalList/TaskImplementation.cs
@@ -6,6 +6,11 @@ using System.Collections.Generic;
 
 public class TaskImplementation : ITask
 {
+    /// <summary>
+    /// Adding a new object of type Task to a database, (to the list of objects of type Task).
+    /// </summary>
+    /// <param name="item">A reference to an existing object of type Task</param>
+    /// <returns>The method will return the running number of the newly created object in the list</returns>
     public int Create(Task item)
     {
         int newId = DataSource.Config.NextTaskId;
@@ -13,49 +18,54 @@ public class TaskImplementation : ITask
         DataSource.Tasks.Add(newTask);
         return newId;
     }
-
+    /// <summary>
+    /// Deletion of an existing object with a certain ID, from the list of objects of type Task
+    /// </summary>
+    /// <param name="id">ID number of an object</param>
+    /// <exception cref="NotImplementedException">An attempt to delete an object that does not exist</exception>
     public void Delete(int id)
     {
-        //לבדוק אם מותר למחוק את היישות
-        // throw new NotImplementedException("An object of type Task with such an ID does not exist");
-        foreach (Task item in DataSource.Tasks)
+        if (DataSource.Tasks.Find(d => d.Id == id) == null)
         {
-            if (item.Id == id)
-            {
-                DataSource.Tasks.Remove(item);
-                return;
-                //אם החלטנו לעשות לא פעיל לעבטד בהתאם
-            }
+            throw new NotImplementedException($"Task with ID={id} does Not exist");
         }
-        throw new Exception($"Student with ID={id} does Not exist");
+        DataSource.Tasks.Remove(DataSource.Tasks.Find(d => d.Id == id)!);
     }
-
+    /// <summary>
+    /// Returning a reference to a single object of type Task with a certain ID, if it exists in
+    /// a database (in a list of data of type Task), or null if the object does not exist.
+    /// </summary>
+    /// <param name="id">ID number of an object</param>
+    /// <returns>If there is an object in the database with the received ID number, the method
+    /// will return a reference to the existing object.Otherwise, the method will return null.</returns>
     public Task? Read(int id)
     {
-        foreach(Task item in DataSource.Tasks)
-        {
-            if(item.Id == id) return item;
-        }
-        return null;
+        return DataSource.Tasks.FirstOrDefault(d => d.Id == id);
     }
-
+    /// <summary>
+    /// Return a copy of the list of references to all objects of type Task
+    /// </summary>
+    /// <returns>The method returns a new list that is a copy of the existing list of all objects of type Task.</returns>
     public List<Task> ReadAll()
     {
         return new List<Task>(DataSource.Tasks);
     }
-
+    /// <summary>
+    /// Update of an existing object. The update will consist of deleting the existing object with the same ID number and replacing it with 
+    /// a new object with the same ID number and updated fields.
+    /// </summary>
+    /// <param name="item">A reference to an updated existing object of type Dependency</param>
+    /// <exception cref="NotImplementedException">In case there is no object with the received id</exception>
     public void Update(Task item)
     {
-        foreach(Task task in DataSource.Tasks)
+        if (DataSource.Tasks.Find(d => d == item) == null)
         {
-            if (task.Id == item.Id)
-            {
-                Task temp= task;
-                DataSource.Tasks.Remove(temp);
-                DataSource.Tasks.Add(item);
-                return;
-            }
+            throw new NotImplementedException($"Task with ID={item.Id} does Not exist");
         }
-        throw new NotImplementedException("An object of type Task with such an ID does not exist");
+        Task task = DataSource.Tasks.Find(d => d == item)!;
+        Task temp = task;
+        DataSource.Tasks.Remove(temp);
+        DataSource.Tasks.Add(item);
+        return;
     }
 }
