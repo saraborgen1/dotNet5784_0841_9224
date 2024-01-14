7e8a5f26f226a407d84bb4f237e2c0c5a835a8cd
Naama Leah Radonsky <radonsky@acad.jct.ac.il>
2024-01-14 20:35:09 +0200

    Stage2 Final commit

--- dotNet5784_0841_9224/DalFacade/DalApi/IDal.cs
+++ dotNet5784_0841_9224/DalFacade/DalApi/IDal.cs
@@ -8,7 +8,16 @@ namespace DalApi;
 
 public interface IDal
 {
+    /// <summary>
+    /// property of task interface
+    /// </summary>
     ITask Task { get; }
+    /// <summary>
+    /// property of dependency interface
+    /// </summary>
     IDependency Dependency { get; }
+    /// <summary>
+    /// property of engineer interface
+    /// </summary>
     IEngineer Engineer { get; }
 }
