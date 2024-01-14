817e305e3f7120013dcb0251406ead1d61c98afd
Naama Leah Radonsky <radonsky@acad.jct.ac.il>
2024-01-14 23:18:03 +0200

    5ב.

---
+++ dotNet5784_0841_9224/DalXml/TaskImplementation.cs
@@ -0,0 +1,35 @@
+﻿namespace Dal;
+using DalApi;
+using DO;
+using System;
+using System.Collections.Generic;
+
+internal class TaskImplementation:ITask
+{
+    readonly string s_task_xml = "task";
+
+    public int Create(Task item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task? Read(Func<Task, bool> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Task?> ReadAll(Func<Task, bool>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Task item)
+    {
+        throw new NotImplementedException();
+    }
+}
