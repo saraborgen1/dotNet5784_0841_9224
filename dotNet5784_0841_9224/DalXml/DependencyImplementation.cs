0f51845e8fa18221736062274060954434b626ae
Naama Leah Radonsky <radonsky@acad.jct.ac.il>
2024-01-15 17:57:22 +0200

    5ב. מימוש המתודות עבור כל ישות

--- dotNet5784_0841_9224/DalXml/DependencyImplementation.cs
+++ dotNet5784_0841_9224/DalXml/DependencyImplementation.cs
@@ -4,8 +4,9 @@ using DalApi;
 using DO;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
-internal class DependencyImplementation: IDependency
+internal class DependencyImplementation : IDependency
 {
     readonly string s_dependencys_xml = "dependencys";
 
@@ -26,10 +27,20 @@ internal class DependencyImplementation: IDependency
 
     public IEnumerable<Dependency?> ReadAll(Func<Dependency, bool>? filter = null)
     {
-        throw new NotImplementedException();
-    }
 
-    public void Update(Dependency item)
+        return filter is null ?
+            XMLTools.LoadListFromXMLElement(s_dependencys_xml).Elements().Select(d => d)
+        : XMLTools.LoadListFromXMLElement(s_dependencys_xml).Elements().Where(filter);
+        //        : XMLTools.LoadListFromXMLElement(s_dependencys_xml).Elements().Select(d => d).Where(filter);
+        //students = (from p in studentRoot.Elements()
+        //            select new Student()
+        //            {
+        //                Id = Convert.ToInt32(p.Element("id").Value),
+        //                FirstName = p.Element("name").Element("firstName").Value,
+        //                LastName = p.Element("name").Element("lastName").Value
+        //            }
+
+        public void Update(Dependency item)
     {
         throw new NotImplementedException();
     }
