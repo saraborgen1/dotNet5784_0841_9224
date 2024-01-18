5ed8368f2389db2169b4203812e76f728a3a52b5
Naama Leah Radonsky <radonsky@acad.jct.ac.il>
2024-01-18 22:31:40 +0200

    תיקון מספר רץ

--- dotNet5784_0841_9224/DalXml/Config.cs
+++ dotNet5784_0841_9224/DalXml/Config.cs
@@ -3,8 +3,8 @@
 internal static class Config
 {
     static string s_data_config_xml = "data-config";
-    internal static int NextTaskId { init; get => GetAndIncreaseNextId(s_data_config_xml, "NextTaskId"); }
-    internal static int NextDependencyId { { get => GetAndIncreaseNextId(s_data_config_xml, "NextDependencyId") private set { } };
+    internal static int NextTaskId { get => GetAndIncreaseNextId(s_data_config_xml, "NextTaskId"); private set { } }
+    internal static int NextDependencyId  { get => GetAndIncreaseNextId(s_data_config_xml, "NextDependencyId"); private set { } }
 
 
 static Config() { NextTaskId = 1; NextDependencyId = 1;}
