e7a004fec4a9a67524e998b5bbc996cab64e3aed
Naama Leah Radonsky <radonsky@acad.jct.ac.il>
2024-01-18 23:18:04 +0200

    תיקון

--- dotNet5784_0841_9224/DalXml/Config.cs
+++ dotNet5784_0841_9224/DalXml/Config.cs
@@ -3,9 +3,9 @@
 internal static class Config
 {
     static string s_data_config_xml = "data-config";
-    internal static int NextTaskId { get => GetAndIncreaseNextId(s_data_config_xml, "NextTaskId"); private set { } }
-    internal static int NextDependencyId  { get => GetAndIncreaseNextId(s_data_config_xml, "NextDependencyId"); private set { } }
+    internal static int NextTaskId { get => GetAndIncreaseNextId(s_data_config_xml, "NextTaskId"); private set; } = 1;
+    internal static int NextDependencyId { get => GetAndIncreaseNextId(s_data_config_xml, "NextDependencyId"); private set; } = 1;
 
 
-static Config() { NextTaskId = 1; NextDependencyId = 1;}
+//static Config() { NextTaskId = 1; NextDependencyId = 1;}
 }
