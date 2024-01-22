c2ea94beb44008907f4e42f9b63f16811e893cdf
שרה בורגן <s.b.01082004@gmail.com>
2024-01-22 21:03:28 +0200

    6ג

--- dotNet5784_0841_9224/BL/BO/Engineer.cs
+++ dotNet5784_0841_9224/BL/BO/Engineer.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BO
+﻿using DO;
+namespace BO;
+public class Engineer
 {
-    internal class Engineer
-    {
-    }
+    public int Id { get; init; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public EngineerExperience? Level { get; set; }
+    public double? Cost { get; set; }
+    public BO.TaskInEngineer? Task { get; set; }
+   
+        
+       
 }
