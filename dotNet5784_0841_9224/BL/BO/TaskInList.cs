9400ec23f09d13b6c30a8226f35299100eeb08dc
שרה בורגן <s.b.01082004@gmail.com>
2024-01-22 21:16:27 +0200

    6ג

--- dotNet5784_0841_9224/BL/BO/TaskInList.cs
+++ dotNet5784_0841_9224/BL/BO/TaskInList.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BO
+namespace BO;
+public class TaskInList
 {
-    internal class TaskInList
-    {
-    }
+    public int Id { get; init; }
+    public string? Alias { get; set; }
+    public string? Description { get; set; }
+    public BO.Status Status { get; set; }
 }
