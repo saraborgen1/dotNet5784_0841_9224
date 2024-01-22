73ce04d23d3042fc731a40f802c5a06bca2dd59a
שרה בורגן <s.b.01082004@gmail.com>
2024-01-22 18:10:07 +0200

    פרק 1 שלב 4

--- dotNet5784_0841_9224/DalList/DalList.cs
+++ dotNet5784_0841_9224/DalList/DalList.cs
@@ -1,7 +1,10 @@
 ﻿namespace Dal;
 using DalApi;
-sealed public class DalList : IDal
+sealed internal class DalList : IDal
 {
+    public static IDal Instance { get; } = new DalList();
+    private DalList() { }
+
     /// <summary>
     /// Interface property implement
     /// </summary>
--- dotNet5784_0841_9224/DalXml/DalXml.cs
+++ dotNet5784_0841_9224/DalXml/DalXml.cs
@@ -1,7 +1,12 @@
-﻿namespace Dal;
+﻿using System.Diagnostics;
 
-public sealed class DalXml : IDal
+namespace Dal;
+
+sealed internal class DalXml : IDal
 {
+    public static IDal Instance { get; } = new DalXml();
+    private DalXml() { }
+
     public ITask Task => new TaskImplementation();
 
     public IDependency Dependency => new DependencyImplementation();
