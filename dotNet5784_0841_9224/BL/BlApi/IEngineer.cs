eea9548a47c1dda5e241bff7134d79f91f7286f9
שרה בורגן <s.b.01082004@gmail.com>
2024-01-26 10:22:45 +0200

    עד שלב 8

--- dotNet5784_0841_9224/BL/BlApi/IEngineer.cs
+++ dotNet5784_0841_9224/BL/BlApi/IEngineer.cs
@@ -2,4 +2,10 @@
 
 public interface IEngineer
 {
+    public IEnumerable<BO.Engineer> ReadAll(Func<BO.Engineer, bool> filter = null!);
+    public BO.Engineer? Read(int id);
+    public void Create(BO.Engineer item);
+    public void Delete(int id);
+    public void Update(BO.Engineer item);
+
 }
--- dotNet5784_0841_9224/BL/BlApi/ITask.cs
+++ dotNet5784_0841_9224/BL/BlApi/ITask.cs
@@ -2,4 +2,11 @@
 
 public interface ITask
 {
+    public IEnumerable<BO.Task> ReadAll(Func<BO.Task, bool> filter = null!);
+    public BO.Task? Read(Func<BO.Task, bool> filter);
+    public void Create(BO.Task item);
+    public void Update(BO.Task item);
+    public void Delete(int id);
+    public void UpdateDate(int id, DateTime date);
+
 }
