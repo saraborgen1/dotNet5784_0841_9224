6ec0ff4b5d9bb11b38f2bc71efcf17c959aa6b25
שרה בורגן <s.b.01082004@gmail.com>
2024-02-01 10:23:20 +0200

    המשך

---
+++ dotNet5784_0841_9224/BL/BlImplementation/Bl.cs
@@ -0,0 +1,8 @@
+﻿namespace BlImplementation;
+using BlApi;
+internal class Bl : IBl
+{
+    public ITask Task => new TaskImplementation();
+
+    public IEngineer Engineer => new EngineerImplementation();
+}
