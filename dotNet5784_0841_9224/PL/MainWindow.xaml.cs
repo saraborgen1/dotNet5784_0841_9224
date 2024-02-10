7eae4bc571946aef5133d50868a001f5c4beae8c
Naama Leah Radonsky <radonsky@acad.jct.ac.il>
2024-02-10 20:17:41 +0200

    עד 5ב

--- dotNet5784_0841_9224/PL/MainWindow.xaml.cs
+++ dotNet5784_0841_9224/PL/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using PL.Engineer;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,11 @@ namespace PL
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            new EngineerListWindow().Show();
         }
     }
 }
