f57f3573a808dbdb89cf876c878ac3d9a592af36
שרה בורגן <s.b.01082004@gmail.com>
2024-02-11 02:03:15 +0200

    עד 7ב

---
+++ dotNet5784_0841_9224/PL/Enums.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+
+namespace PL;
+
+internal class EngineersCollection : IEnumerable
+{
+    static readonly IEnumerable<BO.Enums.EngineerExperience> s_enums =
+(Enum.GetValues(typeof(BO.Enums.EngineerExperience)) as IEnumerable<BO.Enums.EngineerExperience>)!;
+
+    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
+}
+
