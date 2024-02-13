2046db71c4cd8a15940692fca06bdcd98f2b73e8
Naama Leah Radonsky <radonsky@acad.jct.ac.il>
2024-02-13 19:22:54 +0200

    עד 8ד

--- dotNet5784_0841_9224/PL/Engineer/EngineerWindow.xaml.cs
+++ dotNet5784_0841_9224/PL/Engineer/EngineerWindow.xaml.cs
@@ -20,12 +20,32 @@ namespace PL.Engineer
     public partial class EngineerWindow : Window
     {
         static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
-        public EngineerWindow()
+        public EngineerWindow(int id =0)
         {
+            BO.Engineer engineer;
             InitializeComponent();
+            if (id == 0)
+            {
+                engineer = new BO.Engineer();
+            }
+            else
+                try
+                {
+                    engineer = s_bl.Engineer.Read(id)!;
+                }
+                catch (Exception ex) { Console.WriteLine(ex); }
+                
         }
-        public static readonly DependencyProperty EngineerProperty =
-          DependencyProperty.Register("Engineer", typeof(BO.Engineer), typeof(EngineerWindow), new PropertyMetadata(null));
+        public BO.Engineer EngineerProperty
+        {
+            get { return (BO.Engineer)GetValue(EngineerPropertyProperty); }
+            set { SetValue(EngineerPropertyProperty, value); }
+        }
+
+        public static readonly DependencyProperty EngineerPropertyProperty =
+          DependencyProperty.Register("EngineerProperty", typeof(BO.Engineer), typeof(EngineerWindow), new PropertyMetadata(null));
+
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
