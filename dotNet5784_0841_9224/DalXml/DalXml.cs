64b2a5158eec44be5b78f357de7bbcefe4bfe664
Naama Leah Radonsky <radonsky@acad.jct.ac.il>
2024-02-16 14:00:13 +0200

    תיקון

--- dotNet5784_0841_9224/DalXml/DalXml.cs
+++ dotNet5784_0841_9224/DalXml/DalXml.cs
@@ -4,6 +4,15 @@ namespace Dal;
 
 sealed internal class DalXml : IDal
 {
+    public static IDal Instance { get; } = new DalXml();
+    private DalXml() { }
+
+    public ITask Task => new TaskImplementation();
+
+    public IDependency Dependency => new DependencyImplementation();
+
+    public IEngineer Engineer => new EngineerImplementation();
+
     public DateTime? StartProject
     {
         get
@@ -36,12 +45,6 @@ sealed internal class DalXml : IDal
             root.Save(@"..\xml\data_config.xml");
         }
     }
-
-    public ITask Task => new TaskImplementation();
-
-    public IDependency Dependency => new DependencyImplementation();
-
-    public IEngineer Engineer => new EngineerImplementation();
 
     public ProjectStatus StatusProject()
     {
