f174cb22e2ef0e2e556d0c30b3a87163d4be4c0e
שרה בורגן <s.b.01082004@gmail.com>
2024-02-26 20:58:02 +0200

    תיקונים

--- dotNet5784_0841_9224/PL/Enums.cs
+++ dotNet5784_0841_9224/PL/Enums.cs
@@ -10,11 +10,4 @@ internal class EngineersCollection : IEnumerable
     public IEnumerator GetEnumerator() => e_enums.GetEnumerator();
 }
 
-internal class StatusCollection : IEnumerable
-{
-    static readonly IEnumerable<BO.Enums.Status> s_enums =
-(Enum.GetValues(typeof(BO.Enums.Status)) as IEnumerable<BO.Enums.Status>)!;
-
-    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
-}
 
