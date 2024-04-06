6abcbd438c9e672aba39e964a518bbadcf72afd7
Naama Leah Radonsky <radonsky@acad.jct.ac.il>
2024-04-07 00:47:14 +0300

    y

--- dotNet5784_0841_9224/PL/Converter.cs
+++ dotNet5784_0841_9224/PL/Converter.cs
@@ -54,6 +54,18 @@ class ConvertIdToDeleteVisibility : IValueConverter
         throw new NotImplementedException();
     }
 }
+class ConvertIdToPasswordVisibility : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (int)value != 0 ? Visibility.Hidden : Visibility.Visible;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
 
 class ConvertSchedulingIsEnabled : IValueConverter
 {
